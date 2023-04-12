e32aad672f589565d683a1b3c9e19413c9c84c0b
Kgee321 <geek@middleton.school.nz>
2023-04-12 18:36:40 +1200

    Update 06_search_combos_v6.py

--- 06_search_combos_v6.py
+++ 06_search_combos_v6.py
@@ -1,4 +1,4 @@
-""" Component 4 -- Search combos (version 4)
+""" Component 4 -- Search combos (version 6)
 Converting search function into a recyclable code
 so that it focus on searching for the item
 Final code for component 4
@@ -9,6 +9,7 @@ Created on the 05/04/2023
 import easygui
 
 
+# Joining lists together
 def joining_values(lists, join_string):
 
     connected_output = ""
@@ -20,16 +21,19 @@ def joining_values(lists, join_string):
     return connected_output
 
 
-def search(action):
-    # Values
-    value = True
-    search_items = []
-    search_name = []
+# Searching the combos
+def search(items_or_not, action, extra_message):
+
+    delete_items = []
 
     while True:
+        # Values
+        value = True
+        search_items = []
+
         # User enters search
-        searching = easygui.enterbox(f"Enter the combo or combo item you want to {action}: ",
-                                     f"{action} combos").title()
+        searching = easygui.enterbox(f"Enter what you want to {action} in combos:",
+                                     f"{action.title()} Combos").title()
 
         # Loop to access all dictionary items
         for name_com, item_com in combos.items():
@@ -41,26 +45,53 @@ def search(action):
                 items_print = joining_values(item_com.keys(), ", ")
 
                 # printing results
-                search_name.append([name_com, items_print])
+                search_items.append(f"The combo named {name_com} contains {items_print}")
+                delete_items.append(name_com)
                 value = False
 
-            # Loop to access items in combo
-            for item_name in item_com.keys():
+            # If deleting or searching items
+            if items_or_not:
 
-                # If search is a combo item name
-                if searching in item_name:
-                    search_items.append([item_name, item_com[item_name], name_com])
-                    value = False
+                # Loop to access items in combo
+                for item_name in item_com.keys():
+
+                    # If search is a combo item name
+                    if searching in item_name:
+                        search_items.append(f"{item_name} at a price of ${item_com[item_name]} is "
+                                            f"in the combo named {name_com}")
+                        value = False
 
         # Warning message if search not in combos
         if value:
-            easygui.msgbox(f"Sorry, input {searching} is not in the combos",
-                           "Search not in Combos")
+            easygui.msgbox(f"Sorry, input {searching} is not in the combos", "Search not in Combos")
 
         # Printing all search results
         else:
+            # Joining search items together
+            search_total = joining_values(search_items, "\n")
 
-            return [searching, search_items, search_name]
+            # Results output and ask if this is correct to user
+            correct = easygui.buttonbox(f"Combos containing '{searching}' are: \n{search_total}\n"
+                                        f"{extra_message}", "Combo Search Results",
+                                        choices=["Correct", "Incorrect"])
+
+            # if incorrect, component/function restarts
+            if correct == "Incorrect":
+                easygui.msgbox("Sorry, lets try again! It may help if you refine your search "
+                               "by adding as much letters as possible", f"{action} combos again")
+                delete_items.pop(-1)
+                continue
+
+            # Combos found correct
+            easygui.msgbox(f"Great! {action.title()} combo items has been completed!")
+
+        # Asking if user wants to search again
+        play_again = easygui.buttonbox(f"Do you want to {action} again or return to home screen?",
+                                       "Search options", choices=["Again", "Home Screen"])
+
+        # if wanting to return home
+        if play_again == "Home Screen":
+            return delete_items
 
 
 # Pre Stored combos dictionary
@@ -80,39 +111,6 @@ combos = {
 }
 
 # Calling on function for searching dictionary
-while True:
-
-    # Search function
-    answer = search("Search")
-    output_search = ""
-
-    # If search not in combos
-    if answer:
-        pass
-
-    # If search results in combos
-    else:
-
-        # Items that are in search results
-        for i in range(len(answer[1])):
-
-            output_search += f"{answer[1][i][0]} which costs ${answer[1][i][1]} and is in combo "\
-                             f"{answer[1][i][2]} \n"
-
-        # Combo names that are in search results
-        for a in range(len(answer[2])):
-            output_search += f"The combo name {answer[2][a][0]} which contains {answer[2][a][1]}.\n"
-
-        # Output search results
-        easygui.msgbox(f"The search '{answer[0]}' is in: \n{output_search}", "Combo Search Results")
-
-    # User wants to search again or leave to home screen
-    again = easygui.buttonbox(f"Do you want to search again or return to home screen?",
-                              "Again or Home Screen", choices=["Again", "Home Screen"])
-
-    # If wanting to return home
-    if again == "Home Screen":
-        break
-
+search(True, "search", "Are these searched items correct?")
 
 
